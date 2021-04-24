ef6be9b1bcbd11468364cb3817dbd780128cb096
Rick Mann <rmann@latencyzero.com>
2021-04-23 17:56:01 -0700

    Minor cleanup, reordered charts to put dailies up top.

--- vis.js
+++ vis.js
@@ -586,10 +586,12 @@ computePerCapita(ioRegion)
 		return
 	}
 	
+	let dailyCases = []
 	let cases = []
 	let deaths = []
 	ioRegion.confirmed.forEach((c, i) =>
 	{
+		dailyCases.push(ioRegion.confirmed[i] / ioRegion.population)
 		cases.push(ioRegion.confirmed[i] / ioRegion.population)
 		deaths.push(ioRegion.deaths[i] / ioRegion.population)
 	})
@@ -656,7 +658,7 @@ createChart(inElementID, inYAxisLabel, inYFormat, inLegendDataFormat)
 		{
 // 			x: "x",
 			columns: [],
-			empty: { label : { text: "No Data to Display" } },
+			empty: { label : { text: "No Data to Display (or it’s loading…)" } },
 		},
 		axis:
 		{
@@ -664,7 +666,31 @@ createChart(inElementID, inYAxisLabel, inYFormat, inLegendDataFormat)
 			{
 				label: { text: "Day", position: "outer-middle" },
 				type: "timeseries",
-				tick: { format: "%b-%d", values: [ new Date(2020, 0, 22), new Date(2020, 1, 1), new Date(2020, 1, 15), new Date(2020, 2, 1), new Date(2020, 2, 15), new Date(2020, 3, 1), new Date(2020, 3, 15), new Date(2020, 4, 1), new Date(2020, 4, 15), new Date(2020, 5, 1), new Date(2020, 5, 15) ] },
+				tick: { format: "%b-%d", values: [ new Date(2020, 0, 22),
+													new Date(2020, 1, 1),
+													new Date(2020, 2, 1),
+													new Date(2020, 3, 1),
+													new Date(2020, 4, 1),
+													new Date(2020, 5, 1),
+													new Date(2020, 6, 1),
+													new Date(2020, 7, 1),
+													new Date(2020, 8, 1),
+													new Date(2020, 9, 1),
+													new Date(2020, 10, 1),
+													new Date(2020, 11, 1),
+													new Date(2020, 12, 1),
+													new Date(2021, 1, 1),
+													new Date(2021, 2, 1),
+													new Date(2021, 3, 1),
+													new Date(2021, 4, 1),
+													new Date(2021, 5, 1),
+													new Date(2021, 6, 1),
+													new Date(2021, 8, 1),
+													new Date(2021, 9, 1),
+													new Date(2021, 10, 1),
+													new Date(2021, 11, 1),
+													new Date(2021, 12, 1),
+				 ] },
 			},
 			y: {
 				label: { text: inYAxisLabel, position: "outer-middle" },
